a901c3f2dc0f7f2483cb9c6f6e7140521b2223ab
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-05-21 17:47:30 +0900

    1. todo-app - 간단한 To-Do앱 만들기

---
+++ react/todo-app/src/components/Form.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Form({ handleSubmit, value, setValue }) {
+  console.log("<Form /> Component");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="value"
+        className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
+        placeholder="해야 할 일을 입력하세요."
+        value={value}
+        onChange={handleChange}
+      />
+      <input
+        type="submit"
+        value="입력"
+        className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
+      />
+    </form>
+  );
+}
